9c17c708e45ff9446befb7604cd2e78336a9132e
hendisantika <hendisantika@yahoo.co.id>
2024-06-25 07:05:06 +0700

    Create Spring Boot Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootregistrationdemo/SpringBootRegistrationDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootregistrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegistrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRegistrationDemoApplication.class, args);
+    }
+
+}
