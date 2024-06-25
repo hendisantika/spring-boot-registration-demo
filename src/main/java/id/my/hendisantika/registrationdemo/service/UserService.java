2f37f0fc1a019fc5151b9ad58a93bd539ce42626
hendisantika <hendisantika@yahoo.co.id>
2024-06-25 07:11:25 +0700

    Add UserService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/registrationdemo/service/UserService.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.registrationdemo.service;
+
+import id.my.hendisantika.registrationdemo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-registration-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/25/24
+ * Time: 07:10
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+}
