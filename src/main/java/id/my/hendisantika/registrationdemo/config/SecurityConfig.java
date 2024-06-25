54f4f29a5b5853eae203878692103a59b2d8bb32
hendisantika <hendisantika@yahoo.co.id>
2024-06-25 07:17:59 +0700

    Add securityFilterChain in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/registrationdemo/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/registrationdemo/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package id.my.hendisantika.registrationdemo.config;
 
 import id.my.hendisantika.registrationdemo.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,8 +31,41 @@ public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((req) -> req
+                        .requestMatchers("/register").permitAll().requestMatchers("home").permitAll())
+
+
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login") //enable this to go to your own custom login page
+                        .loginProcessingUrl("/login") //enable this to use login page provided by spring security
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error")
+                )
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                );
+
+        return http.build();
+
+    }
+
+
 }
